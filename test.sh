34a60c4e4c9ec8a1136dab22c6607b5944a5e6ff
Thomas Rudin <thomas@rudin.io>
2019-11-26 15:02:07 +0100

    mt 5.1.0

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ docker run --rm -it \
 	-v $(pwd)/test-minetest.conf:/etc/minetest/minetest.conf \
 	-v /tmp/mt:/var/lib/minetest/.minetest \
 	-v $(pwd):/var/lib/minetest/.minetest/worlds/world/worldmods \
-	registry.gitlab.com/minetest/minetest/server:5.0.1
+	registry.gitlab.com/minetest/minetest/server:5.1.0
 
 test -f /tmp/mt/worlds/world/integration_test.json && exit 0 || exit 1
