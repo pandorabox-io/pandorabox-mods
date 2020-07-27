57ed5e142efb8acf236e2d2144efe7217654f6a6
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-07-27 15:02:11 +0200

    test with 5.3.0

--- test.sh
+++ test.sh
@@ -7,6 +7,6 @@ docker run --rm -i \
 	-v $(pwd)/test-minetest.conf:/etc/minetest/minetest.conf \
 	-v /tmp/mt:/var/lib/minetest/.minetest \
 	-v $(pwd):/var/lib/minetest/.minetest/worlds/world/worldmods \
-	registry.gitlab.com/minetest/minetest/server:5.2.0
+	registry.gitlab.com/minetest/minetest/server:5.3.0
 
 test -f /tmp/mt/worlds/world/integration_test.json && exit 0 || exit 1
