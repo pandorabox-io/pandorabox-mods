425aa487c83b67a85daa9d4a375beb19a0fbeb6c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-01-29 14:35:01 +0100

    recursive update

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ git submodule sync
 # initialize new repos
 git submodule init
 # update commits
-git submodule update
+git submodule update --init --recursive
 # remove unversioned files
 git clean -dff
 
